12ceedf72e8055de5c4373fed690d34309a04ac6
purushnahoff <purush.wilson@griffithuni.edu.au>
2013-09-24 20:16:30 +1000

    Second commit from mac

--- Resources/app.js
+++ Resources/app.js
@@ -10,6 +10,7 @@
 	var win1 = Titanium.UI.createWindow({
 		title:"SkateMaps",
 		backgroundImage: '/images/pushing_img1.jpg',
+		backgroundColor: '#000000',
 		navBarHidden: false
 		
 	});
@@ -20,21 +21,21 @@
 		
 	var win2 = Ti.UI.createWindow({
 		title:"SkateMaps",
-		backgroundColor:"#FFFFFF",
+		backgroundColor:"#000000",
 		navBarHidden: false
 		
 	});
 	
 	var win3 = Ti.UI.createWindow({
 		title:"SkateMaps",
-		backgroundColor:"#FFFFFF",
+		backgroundColor: '#000000',
 		navBarHidden: false
 		
 	});
 	
 	var win4 = Ti.UI.createWindow({
 		title:"About",
-		backgroundColor:"#FFFFFF",
+		backgroundColor: '#000000',
 		navBarHidden: false
 		
 	});
@@ -46,6 +47,7 @@
 		
 	});
 	
+	// content for the About page
 	var aboutLabel = Ti.UI.createLabel({
 		color: '#900',
 		text: 'Some info about the app \n-How to use it\n-The developer name\n-Contact email to report bugs',
@@ -78,7 +80,21 @@
 		height: 60,
 		width: 145,
 		top: 350,
-		left: 10
+		left: 10,
+		style: Ti.UI.iPhone.SystemButtonStyle.PLAIN,
+		borderRadius: 10,
+		font:{fontSize:16,fontFamily:'default',fontWeight:'bold'},
+		backgroundGradient: {type: 'linear',
+			colors: [ '#000001', '#666666'],
+			startPoint: {x:0, y:0},
+			endPoint: {x:5, y:80},
+			backFillStart: false},
+		borderWidth:1,
+		borderColor:'#666',
+		selectedColor: '#B200000',
+		//backgroundSelectedImage: '/images/skate_silhouette.jpg'
+			
+		
 	});
 	
 	listButton.addEventListener('click', function(){
@@ -91,12 +107,19 @@
 		height: 60,
 		width: 145,
 		top: 350,
-		right: 10
+		right: 10,
+		style: Ti.UI.iPhone.SystemButtonStyle.PLAIN,
+		borderRadius: 10,
+		font:{fontSize:16,fontFamily:'default',fontWeight:'bold'},
+		backgroundGradient: {type: 'linear',
+			colors: [ '#000001', '#666666'],
+			startPoint: {x:0, y:0},
+			endPoint: {x:5, y:80},
+			backFillStart: false},
+		borderWidth:1,
+		borderColor:'#666',
+		selectedColor: '#B200000'
 	});
-	
-
-	
-
 	
 	
 	var mapview = Ti.Map.createView({
@@ -112,67 +135,28 @@
 	    userLocation:false   
 	});
 	
-	//var parkLocArray = [];
+	// install and open the skateMaps database
 	var db = Ti.Database.install('/mydata/skateMapsDB.sqlite', 'skateMapsDB.sqlite');
 	var tv = Ti.UI.createTableView({minRowHeight:50});
 	var data = [];
 	var parksRS = db.execute('SELECT Id, Name, Address, Rating, Description, Latitude, \
 	 			Longitude, Image_thumb, Image_1, Image_2, Image_3, Image_4 FROM parks');
-	 			
-	var locRS = db.execute('SELECT Id, Name, Address, Latitude, Longitude, FROM parks');
-	// add all park annotations to mapview
-	for (var i = 0; i < locRS.getRowCount(); i++)
-	{
-		mapview.addAnnotation(Ti.Map.createAnnotation({	
-			latitude: locRS.fieldByName('Latitude'),
-			longitude: locRS.fieldByName('Longitude'),
-			title: locRS.fieldByName('Name'),
-			pincolor: Ti.Map.ANNOTATION_RED,
-			animate: true,
-			myId: i + 1	//unique identifyer for this annotation
-		}));
-		locRS.next();
-	}
-	
-	mapButton.addEventListener('click', function(){
-		win5.backButtonTitle = 'Home';
 		
-		// add current location to map
-		mapview.addAnnotation(Ti.Map.createAnnotation({
-			latitude: LATITUDE_BASE,
-			longitude: LONGITUDE_BASE,
-			title: 'You are here!',
-			pincolor: Ti.Map.ANNOTATION_GREEN,
-			animate: true,
-			myId: 0	//unique identifyer for this annotation
-		}));
-			
-		if(mapview !== undefined)
-		{
-			mapview.addEventListener('complete', function(e)
-			{
-				mapview.region = {
-			    	latitude: LATITUDE_BASE, 
-			    	longitude: LONGITUDE_BASE,
-				    latitudeDelta: 0.3, 
-				    longitudeDelta: 0.1
-	  			  };
-			});
-			nav.open(win5, {animated:true});
-		}	
-			
-	});
 	
-
-	
+	//////////////////////////////////////////////////////////////////////
 	// set the list view with data from DB
 	for (var i = 0; i < parksRS.getRowCount(); i++)
 	{
 
-		var row = Ti.UI.createTableViewRow({height:'auto',className:"row"});
+		var row = Ti.UI.createTableViewRow({
+			
+			height: Titanium.UI.SIZE,
+			className:"row",
+			backgroundColor: '#000000'			
+		});
 
 		var nameAndAddressView = Ti.UI.createView({
-			height:'auto',
+			height: Titanium.UI.SIZE,
 			layout:'vertical',
 			left:100,
 			top:10,
@@ -181,7 +165,7 @@
 		});	
 		
 		var descriptionView = Ti.UI.createView({
-			height:'auto',
+			height:Titanium.UI.SIZE,
 			layout:'vertical',
 			left:10,
 			top:85,
@@ -205,45 +189,155 @@
 		
 		var labelAddress = Titanium.UI.createLabel({
 			text: parksRS.fieldByName('Address'),
-			left:0,
-			height:40,
-			width:"100%",
+			font: {
+				fontSize: '12dp',	
+			},
+			left:5,
+			height:50,
+			width:180,
 			textAlign:"left",
 			color:"#FFFFFF",
-			backgroundColor:"#363F45"
+			backgroundColor:"#363F45",
+			
+		});
+			
+		// 
+		var droppinButton = Ti.UI.createButton({
+			backgroundImage: '/images/google_pin.png',
+			top: 5,
+			right: 10,
+			width: 20,
+			height: 35,
 		});
 		
+		var droppinButtonLabel = Titanium.UI.createLabel({	
+			top:0,	
+			right: 0,
+			height:50,
+			width:'100%',
+			backgroundColor:"#363F45"
+		});
+		droppinButtonLabel.add(labelAddress);
+		droppinButtonLabel.add(droppinButton);
+		
+		
+		// star images used for the rating system
+		var star1 = Ti.UI.createImageView({
+			image: '/images/star_yellow1.png',
+			top: 5,
+			left:0,
+			height:25,
+			width:25,
+			backgroundColor:"#363F45"
+			
+		});
+		
+		var star2 = Ti.UI.createImageView({
+			image: '/images/star_yellow1.png',
+			top: 5,
+			left:25,
+			height:25,
+			width:25,
+			backgroundColor:"#363F45"
+			
+		});
+		
+		var star3 = Ti.UI.createImageView({
+			image: '/images/star_yellow1.png',
+			top: 5,
+			left:50,
+			height:25,
+			width:25,
+			backgroundColor:"#363F45"
+			
+		});
+		
+		var star4 = Ti.UI.createImageView({
+			image: '/images/star_yellow1.png',
+			top: 5,
+			left:75,
+			height:25,
+			width:25,
+			backgroundColor:"#363F45"
+			
+		});
+		
+		var star5 = Ti.UI.createImageView({
+			image: '/images/star_yellow1.png',
+			top: 5,
+			left:100,
+			height:25,
+			width:25,
+			backgroundColor:"#363F45"
+			
+		});
+			
 		var labelRating = Titanium.UI.createLabel({
-			text: 'Rating: ' + parksRS.fieldByName('Rating') + ' Stars',
 			top: 15,
 			left:0,
 			height:40,
 			width:"100%",
-			textAlign:"left",
 			color:"#FFFFFF",
+			backgroundColor:"#363F45"
+		});		
+		
+		// determines how many stars for each skatepark and adds them 
+		// to the ratings label
+		(function (){			
+	
+			if (parksRS.fieldByName('Rating') === 1){
+				labelRating.add(star1);	
+			}
+			else if (parksRS.fieldByName('Rating') === 2){
+				labelRating.add(star1,star2);	
+			}
+			else if (parksRS.fieldByName('Rating') === 3){
+				labelRating.add(star1,star2,star3);	
+			}
+			else if (parksRS.fieldByName('Rating') === 4){
+				labelRating.add(star1,star2,star3,star4);	
+			}
+			else if (parksRS.fieldByName('Rating') === 5){
+				labelRating.add(star1,star2,star3,star4,star5);
+			}		
+		})();
+		
+		// grey label behind description text
+		var labelDesc = Titanium.UI.createLabel({	
+			height: Titanium.UI.SIZE,
+			width:'100%',
 			backgroundColor:"#363F45"
 		});
 		
-		var labelDesc = Titanium.UI.createLabel({
+		// description text on top of above lableDesc
+		var descText = Titanium.UI.createLabel({
 			text: parksRS.fieldByName('Description'),
-			left:0,
-			height:'auto',
-			width:"100%",
+			font: { fontSize: '12dp'},
+			top:0,
+			left:5,
+			right:5,
+			bottom:5,
+			height: Titanium.UI.SIZE,
+			width:Titanium.UI.SIZE,
 			textAlign:"left",
 			color:"#FFFFFF",
 			backgroundColor:"#363F45"
-		});
+		});			
+		labelDesc.add(descText);	
 		
-				
+		// thumnbnail image of skatepark		
 		var imageThumb = Ti.UI.createImageView({
 			image: parksRS.fieldByName('Image_1'),
 			left:10,
 			top:10,
 			height:80,
-			width:80
+			width:80,
+			borderWidth:2,
+			borderRadius: 3,
+			borderColor: "#FFFFFF"		
 		});
 		
-		
+		// when image is clicked it opens a photoscroll of larger images
 		imageThumb.addEventListener('click', function(e){
 			var imagePath = new String(e.source.image);
 			imagePath = imagePath.replace("1.jpg", "");
@@ -318,8 +412,10 @@
 			nav.open(win3, {animated:true});
 		});
 		
+		nameAndAddressView.add(labelName);
+		nameAndAddressView.add(droppinButtonLabel);
+		//nameAndAddressView.add(labelAddress);
 		
-		nameAndAddressView.add(labelName, labelAddress);
 		descriptionView.add(labelRating, labelDesc);
 		row.add(nameAndAddressView, descriptionView);
 		row.add(imageThumb);
@@ -329,10 +425,74 @@
 		
 	}
 	parksRS.close();
+	db.close();
 	tv.setData(data);
 	
-
+	// 
+	mapButton.addEventListener('click', function(){
+		win5.backButtonTitle = 'Home';
+		
+		db = Ti.Database.open('skateMapsDB.sqlite');
+		var locRS = db.execute('SELECT * FROM parks');
+		
+		// add all park annotations to mapview
+		for (var i = 0; i < locRS.getRowCount(); i++)
+		{
+			mapview.addAnnotation(Ti.Map.createAnnotation({	
+				latitude: locRS.fieldByName('Latitude'),
+				longitude: locRS.fieldByName('Longitude'),
+				title: locRS.fieldByName('Name'),
+				pincolor: Ti.Map.ANNOTATION_RED,
+				animate: true,
+				leftButton: Ti.UI.iPhone.SystemButton.INFO_LIGHT,
+				myid: i + 1	//unique identifyer for this annotation
+			}));
+			locRS.next();
+		}
+			
+		// add current location to map
+		mapview.addAnnotation(Ti.Map.createAnnotation({
+			latitude: LATITUDE_BASE,
+			longitude: LONGITUDE_BASE,
+			title: 'You are here!',
+			pincolor: Ti.Map.ANNOTATION_GREEN,
+			animate: true,
+			myid: 0	//unique identifyer for this annotation
+		}));
+			
+		if(mapview !== undefined)
+		{
+			mapview.addEventListener('complete', function(e)
+			{
+				mapview.region = {
+			    	latitude: LATITUDE_BASE, 
+			    	longitude: LONGITUDE_BASE,
+				    latitudeDelta: 0.3, 
+				    longitudeDelta: 0.1
+	  			  };
+			});
+			nav.open(win5, {animated:true});
+		}	
+			
+	});
+	db.close();
 	win5.add(mapview);
+	
+	mapview.addEventListener('click', function(e){
+		Ti.API.info("Annotation " + e.title + " clicked, id: " + e.annotation.myid);
+		
+		if (e.clicksource == 'leftButton' || e.clicksource == 'leftPane' ||
+		e.clicksource == 'leftView'){
+			Ti.API.info("Annotation " + e.title + ", left button clicked.");
+		}
+		
+	});
+	
+	
+				
+/////////////////////////////////////////////////////////////////////////////				
+				
+	
 		
 	win2.add(tv);
 	win1.add(listButton);
